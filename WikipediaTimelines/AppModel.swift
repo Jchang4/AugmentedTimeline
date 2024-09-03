f41cbddb803f3545ad8e997a616a0d07a721e587
Justin <justinchangmusic@gmail.com>
2024-09-03 18:15:21 -0400

    Remove unnecessary files

--- WikipediaTimelines/AppModel.swift
+++ WikipediaTimelines/AppModel.swift
@@ -45,17 +45,3 @@ class GlobalAppState {
         return prevHeight > 0.1 ? -0.4 : 0.4
     }
 }
-
-/// Maintains app-wide state
-@MainActor
-@Observable
-class AppModel {
-    let immersiveSpaceID = "ImmersiveSpace"
-    enum ImmersiveSpaceState {
-        case closed
-        case inTransition
-        case open
-    }
-
-    var immersiveSpaceState = ImmersiveSpaceState.closed
-}
--- WikipediaTimelines/WikipediaTimelinesApp.swift
+++ WikipediaTimelines/WikipediaTimelinesApp.swift
@@ -9,21 +9,9 @@ import SwiftUI
 
 @main
 struct WikipediaTimelinesApp: App {
-    @State private var appModel = AppModel()
-    @State private var avPlayerViewModel = AVPlayerViewModel()
-
     var body: some Scene {
         ImmersiveSpace(id: "TimelineImmersive") {
             ImmersiveView()
-                .environment(appModel)
-                .onAppear {
-                    appModel.immersiveSpaceState = .open
-                    avPlayerViewModel.play()
-                }
-                .onDisappear {
-                    appModel.immersiveSpaceState = .closed
-                    avPlayerViewModel.reset()
-                }
         }
         .immersionStyle(selection: .constant(.mixed), in: .mixed)
     }
